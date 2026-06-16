acc04d2a90bd7a9c5fc793743ce66bb16b992ead
dungkkk7 <dungvn2345@gmail.com>
2026-06-16 19:01:14 +0700

    Ignore src/ and result/ only, track other dependencies and files

---
+++ dependency/Simplifier/EqSat/target/release/deps/log-b732e2c8fb16a00a.d
@@ -0,0 +1,10 @@
+/home/dungbv/WorkSpace/omill/third_party/Simplifier/EqSat/target/release/deps/log-b732e2c8fb16a00a.d: /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
+
+/home/dungbv/WorkSpace/omill/third_party/Simplifier/EqSat/target/release/deps/liblog-b732e2c8fb16a00a.rlib: /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
+
+/home/dungbv/WorkSpace/omill/third_party/Simplifier/EqSat/target/release/deps/liblog-b732e2c8fb16a00a.rmeta: /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs /home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs
+
+/home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/lib.rs:
+/home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/macros.rs:
+/home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/serde.rs:
+/home/dungbv/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/log-0.4.29/src/__private_api.rs:
---
+++ dependency/remill/lib/cmake/remill/remillTargets-release.cmake
@@ -0,0 +1,99 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "remill_arch_aarch32" for configuration "Release"
+set_property(TARGET remill_arch_aarch32 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_arch_aarch32 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_arch_aarch32.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_arch_aarch32 )
+list(APPEND _cmake_import_check_files_for_remill_arch_aarch32 "${_IMPORT_PREFIX}/lib/libremill_arch_aarch32.a" )
+
+# Import target "remill_arch_aarch64" for configuration "Release"
+set_property(TARGET remill_arch_aarch64 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_arch_aarch64 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_arch_aarch64.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_arch_aarch64 )
+list(APPEND _cmake_import_check_files_for_remill_arch_aarch64 "${_IMPORT_PREFIX}/lib/libremill_arch_aarch64.a" )
+
+# Import target "remill_arch_sparc32" for configuration "Release"
+set_property(TARGET remill_arch_sparc32 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_arch_sparc32 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_arch_sparc32.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_arch_sparc32 )
+list(APPEND _cmake_import_check_files_for_remill_arch_sparc32 "${_IMPORT_PREFIX}/lib/libremill_arch_sparc32.a" )
+
+# Import target "remill_arch_sparc64" for configuration "Release"
+set_property(TARGET remill_arch_sparc64 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_arch_sparc64 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_arch_sparc64.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_arch_sparc64 )
+list(APPEND _cmake_import_check_files_for_remill_arch_sparc64 "${_IMPORT_PREFIX}/lib/libremill_arch_sparc64.a" )
+
+# Import target "remill_arch_x86" for configuration "Release"
+set_property(TARGET remill_arch_x86 APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_arch_x86 PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_arch_x86.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_arch_x86 )
+list(APPEND _cmake_import_check_files_for_remill_arch_x86 "${_IMPORT_PREFIX}/lib/libremill_arch_x86.a" )
+
+# Import target "remill_arch" for configuration "Release"
+set_property(TARGET remill_arch APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_arch PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_arch.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_arch )
+list(APPEND _cmake_import_check_files_for_remill_arch "${_IMPORT_PREFIX}/lib/libremill_arch.a" )
+
+# Import target "remill_bc" for configuration "Release"
+set_property(TARGET remill_bc APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_bc PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_bc.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_bc )
+list(APPEND _cmake_import_check_files_for_remill_bc "${_IMPORT_PREFIX}/lib/libremill_bc.a" )
+
+# Import target "remill_os" for configuration "Release"
+set_property(TARGET remill_os APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_os PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_os.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_os )
+list(APPEND _cmake_import_check_files_for_remill_os "${_IMPORT_PREFIX}/lib/libremill_os.a" )
+
+# Import target "remill_version" for configuration "Release"
+set_property(TARGET remill_version APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(remill_version PROPERTIES
+  IMPORTED_LINK_INTERFACE_LANGUAGES_RELEASE "CXX"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/lib/libremill_version.a"
+  )
+
+list(APPEND _cmake_import_check_targets remill_version )
+list(APPEND _cmake_import_check_files_for_remill_version "${_IMPORT_PREFIX}/lib/libremill_version.a" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
