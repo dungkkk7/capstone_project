df83ef0379b5154965c4d1de0910bc55e7a32794
Bùi Việt Dũng <117715149+dungkkk7@users.noreply.github.com>
2026-08-18 15:24:03 +0700

    fix runtime verifier diagnostics include

--- src/llvm_pass/brighten_015_runtime_helper_materialization/src/main.cpp
+++ src/llvm_pass/brighten_015_runtime_helper_materialization/src/main.cpp
@@ -4,6 +4,7 @@
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 #include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/raw_ostream.h"
 
 namespace brighten_runtime {
 
